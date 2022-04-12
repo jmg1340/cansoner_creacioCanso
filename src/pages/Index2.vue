76d657afe6f994a0fc68942ed81551fb834f819d
jordi <jordi.miserachs@gmail.com>
2022-04-12 13:59:26 +0200

    lletra cançó en un textArea

---
+++ src/pages/Index2.vue
@@ -0,0 +1,360 @@
+<template>
+  <q-page class="">
+
+    <div class="row">
+      <div class="col">
+        <q-btn class="q-ma-sm" color="negative" label="Eliminar dades" noCaps @click="eliminarDades()"/>
+      </div>
+      <div class="col text-right">
+        <q-btn class="q-ma-sm" color="orange" label="Copiar" noCaps @click="copiarDades()"/>
+      </div>
+    </div>
+    
+    
+    <div class="row q-ma-md q-gutter-md">
+      <div class="col-5" style="min-width: 0">
+        <div class="column">
+          
+          <q-card class="q-pa-md q-mb-md">
+            <div class="row items-center items-center">
+              <div class="col-2 text-negative text-negative">Idioma:</div>
+              <div class="col">
+                <q-select  v-model="canso.idioma" :options="opcionsIdioma" dense/>
+              </div>
+            </div>
+
+
+            <div class="row items-center">
+              <div class="col-2 text-negative">Titol:</div>
+              <div class="col">
+                <q-input dense  v-model="canso.titol" />
+              </div>
+            </div>
+
+            <div class="row items-center">
+              <div class="col-2 text-negative">Audio src:</div>
+              <div class="col">
+                <q-input dense  v-model="canso.audiosrc" />
+              </div>
+            </div>
+  
+            <div class="row items-center">
+              <div class="col-3 text-negative">Estat:</div>
+              <div class="col">
+                <q-select  v-model="canso.estat" :options="opcionsEstat" dense/>
+                <!-- <q-input dense v-model="canso.estat" /> -->
+              </div>
+            </div>
+
+            <div class="row items-center">
+              <div class="col-4 text-negative">Cansoner - llibre</div>
+              <div class="col">
+                <q-select  v-model="canso.cansoner.nom" :options="opcionsCansoner" dense/>
+                <!-- <q-input dense v-model="canso.cansoner.nom" /> -->
+              </div>
+            </div>
+
+            <div class="row items-center">
+              <div class="col-4 text-negative">Cansoner - numero</div>
+              <div class="col">
+                <q-input dense v-model="canso.cansoner.numero" />
+              </div>
+            </div>
+          </q-card>
+          
+          
+          <!-- lletra de la cançó -->
+
+					<q-card>
+						<q-input dense  v-model="txtAreaLletra" filled type="textarea" autogrow/>
+
+					</q-card>
+
+
+          <!-- <div v-for="(obj, i) in lletraCanso" :key="i">
+            <cmpParagraf 
+              :obj="obj"
+              :idx ="i"
+              @eventNouTipus="handlerNouTipus"
+              @eventNouParagraf="handlerNouParagraf"
+              />
+              
+          </div>
+          <q-btn class="col-1 text-white bg-teal" label="Nou Paragraf" noCaps @click="nouParagraf()"/> -->
+        
+        
+        </div>
+        
+
+      </div>
+
+      <!-- INFORMACIO DEL OBJECTE RESULTANT -->
+      <div class="col">
+        <pre>{{ canso2 }}</pre>
+      </div>
+      
+    </div>
+
+
+
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    > -->
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref, computed } from 'vue';
+import { copyToClipboard } from 'quasar'
+
+import cmpParagraf from '../components/paragraf.vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { cmpParagraf },
+
+  setup() {
+
+    let canso = ref({
+      idioma: {label: "", value: null},
+      titol: null,
+      audiosrc: "",
+      estat: {label: "", value: null},
+      cansoner: {
+        nom: {label: "", value: null},
+        numero: null
+      },
+      lletra: [
+        // {
+        //   tipus: "estrofa" / "tornada",
+        //   paragraf: []
+        // }
+      ]
+    })
+
+		let txtAreaLletra = ref(`
+		aixo es la linia numero 11
+		aixo es la linia numero 12
+		aixo es la linia numero 13
+		aixo es la linia numero 14
+		
+
+		aixo es la linia numero 21
+		aixo es la linia numero 22
+		aixo es la linia numero 23
+		aixo es la linia numero 24
+
+		`);
+
+
+    const eliminarDades = () => {
+      canso.value = {
+        idioma: {label: "", value: null},
+        titol: "",
+        audiosrc: "",
+        estat: {label: "", value: null},
+        cansoner: {
+          nom: {label: "", value: null},
+          numero: null
+        },
+        lletra: [
+          // {
+          //   tipus: "estrofa" / "tornada",
+          //   paragraf: []
+          // }
+        ],
+				txtAreaLletra: ""
+      }
+      lletraCanso.value = []
+    }
+
+    const copiarDades = () => {
+      copyToClipboard(document.querySelector('pre').innerHTML)
+      .then(() => {
+        // success!
+      })
+      .catch((error) => {
+        console.log("error al copiar", error)
+      })      
+    }
+
+    const opcionsIdioma = ref ([
+      {label: "CASTELLÀ", value: "ES"},
+      {label: "CATALÀ", value: "CAT"}
+    ])
+
+    const opcionsCansoner = ref ([
+      {label: "BLAU", value: "blau"},
+      {label: "VERMELL", value: "vermell"}
+    ])
+
+    const opcionsEstat = ref ([
+      {label: "NOVA", value: "nova"},
+      {label: "", value: null}
+    ])
+
+
+
+
+    //--- LLETRA CANÇÓ -----
+
+const lletraCanso = computed ( () => {
+	
+	let arrTxtArea = txtAreaLletra.value.split("\n")
+	
+	let arrParagraf = []
+	let arrParagrafs = arrTxtArea.reduce ( (acc, linia, index, arr) =>{
+
+		linia = linia.replace(/[\t]+/g, '')  // treiem les \t si en te
+		linia = linia.trim()
+		console.log("==========================")
+		console.log("LINIA: ", "'"+linia+"'")
+
+		if (index === 0 ) {     // si és 1ª linia 
+			console.log("estic al PRIMER element de arr")
+			if (linia.length !== 0) arrParagraf.push(linia)
+			console.log("index:", index, "arrParagraf:", arrParagraf )
+			return acc
+
+		} else if (index === arr.length - 1  &&  arrParagraf.length !== 0 ) {  // si és ultima linia i arrParagraf.length !== 0 fer un ultim push
+			console.log("estic al ULTIM  element de arr i arrParagraf.length no és zero")
+			arrParagraf.push(linia)
+			console.log("index:", index, "arrParagraf:", arrParagraf )
+			acc.push(arrParagraf)
+			return acc
+
+		} else if (linia.length !== 0) {
+			arrParagraf.push(linia)
+			console.log("linia.length !== 0")
+			console.log("index:", index, "arrParagraf:", arrParagraf )
+			return acc
+
+		} else {
+			console.log("ELSE")
+			console.log("index:", index, "arrParagraf:", arrParagraf )
+			if (arrParagraf.length !== 0) acc.push(arrParagraf)
+			arrParagraf = []
+			console.log("index:", index, "arrParagraf:", arrParagraf )
+		return acc
+		}
+
+		
+
+	}, [])
+	
+	
+
+	/* Per cada paragraf creem un objecte
+			{
+				tipus: "estrofa" / "tornada"
+				paragraf: [paragraf]
+			}
+	*/
+
+	const arrLletra = arrParagrafs.map( arrParagraf => {
+		let arrP = []
+		if (arrParagraf[0].toLowerCase() === "t" ){
+			arrP = arrParagraf.slice(1)
+		} else {
+			arrP = arrParagraf
+		}
+
+		return { 
+			tipus: (arrParagraf[0].toLowerCase() === "t") ? "tornada" : "estrofa" ,
+			paragraf: arrP
+		}
+	})
+
+	
+	return [arrLletra]
+})
+
+
+
+
+/* 
+    const lletraCanso = ref([])
+
+    const nouParagraf = () => lletraCanso.value.push({
+      tipus: "estrofa",
+      paragraf: ""
+    })
+
+    const handlerNouTipus = (objT) => {
+      console.log("tipus", objT)
+      lletraCanso.value[objT.index].tipus = objT.tipus
+    }
+
+    const handlerNouParagraf = (objP) => {
+      console.log("objP", objP)
+
+      let arr = objP.paragraf.split("\n")
+      arr = arr.filter( (linia) => linia.length !== 0 )
+      lletraCanso.value[objP.index].paragraf = arr
+    }
+ */
+
+
+
+    // ----- OBJECTE RESULTAT ------
+
+    const canso2 = computed( () => {
+      console.log("estic a GeneraObjecteCanso")
+
+      let obj = {}
+
+      obj[canso.value.idioma.value] = {
+        titol: canso.value.titol,
+        
+        audio: ((src) => {
+            console.log("estic dins funcio audio")
+            if (src.length === 0) {
+              return null
+            } else {
+              return [
+                  {
+                    src: src,
+                    type: "audio/mp3"
+                  }
+                ]
+            }
+          })(canso.value.audiosrc),
+
+        estat: canso.value.estat.value || null,
+        cansoner: {
+          nom : canso.value.cansoner.nom.value,
+          numero : canso.value.cansoner.numero
+        },
+        lletra: lletraCanso.value
+      }
+
+
+      console.log("obj", obj)
+
+      return obj
+    } )
+
+
+
+
+    return { 
+      canso,
+      eliminarDades,
+      copiarDades,
+
+      opcionsIdioma,
+      opcionsCansoner,
+      opcionsEstat,
+      lletraCanso,
+      // nouParagraf,
+      canso2,
+			txtAreaLletra,
+
+      // handlerNouTipus,
+      // handlerNouParagraf
+    }
+  }
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,13 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
+      { path: '', component: () => import('pages/Index2.vue') }
+    ]
+  },
+  {
+    path: '/2',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
